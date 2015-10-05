9eb6ed939e9da09e49676196c56d84b846edca02
Matthew_2 <Matthew_2@10.30.19.122>
2015-10-05 16:33:38 -0500

    4rd Commit. - started to work on the reciept class not quite sure if i am doing this right.

--- src/discountstrategy/Reciept.java
+++ src/discountstrategy/Reciept.java
@@ -10,5 +10,38 @@ package discountstrategy;
  * @author mparish2
  */
 public class Reciept {
+   DatabaseAccessStrategy db = new FakeDataBase();
     
-}
+   private Customer [] cust;
+   
+   private Product [] p;
+   
+   private LineItem [] li;
+
+    public Customer[] getCust() {
+        return cust;
+    }
+
+    public void setCust(Customer[] cust) {
+        this.cust = cust;
+    }
+
+    public Product[] getP() {
+        return p;
+    }
+
+    public void setP(Product[] p) {
+        this.p = p;
+    }
+
+    public LineItem[] getLi() {
+        return li;
+    }
+
+    public void setLi(LineItem[] li) {
+        this.li = li;
+    }
+   
+   
+   
+} 
