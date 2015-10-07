bb1f7e9b70b6786a46e58394b87e9c9e23848c59
Matthew_2 <Matthew_2@10.30.18.131>
2015-10-07 14:51:34 -0500

    5th commit- finished it expect for adding a date. Also possibley adding a outputStrategy

--- src/discountstrategy/Start.java
+++ src/discountstrategy/Start.java
@@ -15,7 +15,20 @@ public class Start {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        CashRegister cR = new CashRegister();
+
+        // Customer 1
+        cR.startSale("300", new FakeDataBase());
+        cR.addItem("B205", 2);
+        cR.addItem("A101", 1);
+        cR.endSaleAndDisplayReciept();
+
+        // Customer 2
+        cR.startSale("100", new FakeDataBase());
+        cR.addItem("C222", 4);
+        cR.addItem("B205", 6);
+        cR.endSaleAndDisplayReciept();
     }
-    
 }
+
+
